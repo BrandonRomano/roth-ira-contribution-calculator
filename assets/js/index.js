21a031899c9993490ca45225ed1136f9b1a40b7c
BrandonRomano <BrandonRRomano@gmail.com>
2018-01-24 23:17:50 -0800

    Updates for 2017 tax year

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,5 +1,17 @@
 import Vue from 'vue'
 
+
+// Jointly or Widow
+const jointPhaseOutBegin = 186000
+const jointPhaseOutEnd = 196000
+
+// Single
+const singlePhaseOutBegin = 118000
+const singlePhaseOutEnd = 133000
+
+// Married Filing separately + lived with eachother
+const mfsCutoff = 10000
+
 new Vue({
   el: '#roth-calculator',
   data: {
@@ -18,27 +30,27 @@ new Vue({
       }
 
       if(this.filing_status === 'jointly' || this.filing_status === 'widow') {
-        if(this.magi < 184000) {
+        if(this.magi < jointPhaseOutBegin) {
           this.result = Math.min(this.taxable_compensation, base_limit);
-        } else if(this.magi >= 184000 && this.magi < 194000) {
-          this.result = (1.0  - ((this.magi - 184000) / 10000)) * base_limit
+        } else if(this.magi >= jointPhaseOutBegin && this.magi < jointPhaseOutEnd) {
+          this.result = (1.0  - ((this.magi - jointPhaseOutBegin) / (jointPhaseOutEnd - jointPhaseOutBegin))) * base_limit
         } else {
           this.result = 0
         }
       }
       else if(this.filing_status === 'mfs-lived-with') {
-        if(this.magi < 10000) {
-          var max_result = (1.0  - (this.magi / 10000)) * base_limit
+        if(this.magi < mfsCutoff) {
+          var max_result = (1.0  - (this.magi / mfsCutoff)) * base_limit
           this.result = Math.min(this.taxable_compensation, max_result)
         } else {
           this.result = 0
         }
       }
       else if(this.filing_status === 'single' || this.filing_status === 'mfs-lived-without') {
-        if(this.magi < 117000) {
+        if(this.magi < singlePhaseOutBegin) {
           this.result = Math.min(this.taxable_compensation, base_limit);
-        } else if(this.magi >= 117000 && this.magi < 132000) {
-          this.result = (1.0  - ((this.magi - 117000) / 15000)) * base_limit
+        } else if(this.magi >= singlePhaseOutBegin && this.magi < singlePhaseOutEnd) {
+          this.result = (1.0  - ((this.magi - singlePhaseOutBegin) / (singlePhaseOutEnd - singlePhaseOutBegin))) * base_limit
         } else {
           this.result = 0
         }
